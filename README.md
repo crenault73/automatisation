ad52f578a87cdcb6fc7c161b1aba84bea6ebf611
akloul amira <amira.akloul@ens.uvsq.fr>
2020-10-07 23:27:46 +0200

    Tests selenium initiaux

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Automatisation
 
 Automatisation de tests
+
+Prérquis : 
+
+Installer Maven
+Installer JDK
+Installer Java
+
+créer un web driver 
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,58 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    public static WebDriver driver;
+
+    public static void tempo(long delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After()
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+
+    @Test
+    public void testSite() {
+        driver.get("http://the-internet.herokuapp.com/");
+        //<h1 class="heading">Welcome to the-internet</h1>
+        WebElement title = driver.findElement(By.xpath("//h1"));
+        System.out.println("Contenu de title: " + title.getText());
+        Assert.assertEquals("Welcome to the-internet", title.getText());
+
+        List<WebElement> list = driver.findElements(By.xpath("//ul/li/a [contains(@href, 'dynamic')]"));
+
+        //List<WebElement> interventionFound = (List<WebElement>) interventions.stream() .filter(we -> we.getText().contains(title)) .collect(Collectors.toList());
+
+        WebElement image = driver.findElement(By.xpath("//img[@alt=\'Fork me on GitHub\']"));
+        image.click();
+        AppTest.tempo(3000);
+        driver.quit();
+
+    }
+
+
+}
